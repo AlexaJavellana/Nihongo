126055fe9bb02e39ee87c56535824040ef3461f6
Alexa Javellana <Alexa.Javellana@ibm.com>
2018-08-14 09:12:48 -0400

    Test file for script program

---
+++ test.py
@@ -0,0 +1,20 @@
+import glob
+import os 
+
+print('All files in the documents list')
+
+particleFiles = []
+for prtPath in glob.glob('/Users/alexaj/Documents/PRT*.tiff'):
+  print prtPath
+  filename =  os.path.basename(prtPath)
+  print(filename)
+  # lessonFiles.append(filename)
+  
+
+lessonFiles = []
+for lsnPath in glob.glob('/Users/alexaj/Documents/*B*.tiff'):
+  print lsnPath
+  filename = os.path.basename(lsnPath)
+  print(filename)
+  # lessonFiles.append(filename)
+
