04b563fd410dc56462c70622810e8c69a6aee9df
Alexa Javellana <Alexa.Javellana@ibm.com>
2018-08-14 09:24:22 -0400

    read me changes

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ print('All files in the documents list')
 
 particleFiles = []
 for prtPath in glob.glob('/Users/alexaj/Documents/PRT*.tiff'):
-  print prtPath
+  # print prtPath
   filename =  os.path.basename(prtPath)
   print(filename)
   # lessonFiles.append(filename)
@@ -13,7 +13,7 @@ for prtPath in glob.glob('/Users/alexaj/Documents/PRT*.tiff'):
 
 lessonFiles = []
 for lsnPath in glob.glob('/Users/alexaj/Documents/*B*.tiff'):
-  print lsnPath
+  # print lsnPath
   filename = os.path.basename(lsnPath)
   print(filename)
   # lessonFiles.append(filename)
